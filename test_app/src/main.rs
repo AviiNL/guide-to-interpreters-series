43650021eabc6ebf8185bf9f4fe7968184d723fe
AviiNL <me@avii.nl>
2022-11-12 23:41:46 +0100

    cleanup on repl

--- test_app/src/main.rs
+++ test_app/src/main.rs
@@ -3,12 +3,8 @@ use std::io::Write;
 use avii_lang::parser::Parser;
 
 fn main() {
+    println!("REPL {}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
 
-    let prompt = env!("CARGO_PKG_NAME").to_string() + " " + env!("CARGO_PKG_VERSION");
-
-    println!("{}", prompt);
-
-    // REPL with "> " prompt
     loop {
         let mut input = String::new();
         print!("> ");
@@ -17,10 +13,4 @@ fn main() {
         let ast = Parser::produce_ast(&input);
         println!("{:#?}", ast);
     }
-
-
-
-    // let parser = Parser::produce_ast(src);
-
-    // println!("{:#?}", parser);
 }
